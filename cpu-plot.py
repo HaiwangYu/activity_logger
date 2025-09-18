6ff0febeb68ab5518997d1530c863b98dc40f115
HaiwangYu <yuhw.pku@gmail.com>
2025-09-18 09:28:25 -0500

    no pandas

--- cpu-plot.py
+++ cpu-plot.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
 import matplotlib
 print("Backend used by matplotlib is: ", matplotlib.get_backend())
 
 inputs = [
-"log",
+    "log",
+    # "trad.log",
+    # "dnn-haiwang-nchunk1.log",
+    # "dnn-haiwang-nchunk2.log",
+    # "dnn-haiwang-nchunk4.log",
+    # "dnn-mobilenetv3-nchunk1.log",
+    # "dnn-mobilenetv3-nchunk2.log",
 ]
 labels = inputs
 linestyles = [
@@ -24,31 +29,34 @@ linestyles = [
 "-",
 ]
 
+# Function to replace pandas read_csv
+def read_csv_file(filename, sep=' '):
+    class SimpleDataFrame:
+        def __init__(self, data_array):
+            self.data = data_array
+            self.shape = data_array.shape
+        
+        def __getitem__(self, idx):
+            # Return the column at index idx
+            return self.data[:, idx]
+    
+    try:
+        data = np.loadtxt(filename, delimiter=sep)
+        return SimpleDataFrame(data)
+    except Exception as e:
+        print(f"Error reading file {filename}: {e}")
+        return None
 
-dfs = [pd.read_csv(tag, sep=' ', header=None) for tag in inputs]
+dfs = [read_csv_file(tag) for tag in inputs]
 xscale = 9./24
 xscale = 204./740
 cpu_lim = [0, 200]
-ram_size = 251 # 31.3, 10
-ram_lim = [0, 5]
+ram_size = 92.5 # 31.3, 10
+ram_lim = [0, 10]
 
 fontsize = 18
 
-for i in range(len(inputs)) :
-    x = np.linspace(0,dfs[i][0].shape[0]*xscale,dfs[i][0].shape[0])
-    plt.plot(x,dfs[i][0],label=labels[i]
-        , linestyle=linestyles[i]
-    )
-plt.legend(loc='best',fontsize=fontsize)
-plt.grid()
-plt.ylim(cpu_lim)
-plt.xlabel("time [sec]", fontsize=fontsize)
-plt.ylabel("cpu [%]", fontsize=fontsize)
-plt.yticks(fontsize=fontsize)
-plt.xticks(fontsize=fontsize)
-plt.tight_layout()
-plt.show()
-
+# memory in GB
 for i in range(len(inputs)) :
     x = np.linspace(0,dfs[i][0].shape[0]*xscale,dfs[i][0].shape[0])
     plt.plot(x,dfs[i][1]*ram_size/100,label=labels[i]
@@ -60,6 +68,23 @@ plt.grid()
 plt.ylim(ram_lim)
 plt.xlabel("time [sec]", fontsize=fontsize)
 plt.ylabel("memory [GB]", fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.xticks(fontsize=fontsize)
+plt.tight_layout()
+plt.show()
+exit()
+
+# cpu in %
+for i in range(len(inputs)) :
+    x = np.linspace(0,dfs[i][0].shape[0]*xscale,dfs[i][0].shape[0])
+    plt.plot(x,dfs[i][0],label=labels[i]
+        , linestyle=linestyles[i]
+    )
+plt.legend(loc='best',fontsize=fontsize)
+plt.grid()
+plt.ylim(cpu_lim)
+plt.xlabel("time [sec]", fontsize=fontsize)
+plt.ylabel("cpu [%]", fontsize=fontsize)
 plt.yticks(fontsize=fontsize)
 plt.xticks(fontsize=fontsize)
 plt.tight_layout()
--- top.sh
+++ top.sh
@@ -3,5 +3,5 @@
 #while sleep 1; do  ps -C $1 -o pcpu= -o pmem= -o nlwp=; done;
 #while sleep 1; do top -b -n 1 | awk '/wire-cell/ {print $9,$10}'; done;
 
-while sleep 0.1; do COLUMNS=200 top -u yuhw -b -n 1 | awk -v pattern="$1" '$0 ~ pattern {print $9,$10}' | head -1; done;
+while sleep 0.1; do COLUMNS=200 top -u $USER -b -n 1 | awk -v pattern="$1" '$0 ~ pattern {print $9,$10}' | head -1; done;
 #while sleep 0.25; do top -b -n 1 | awk -v pattern="$1" '$0 ~ pattern {print $9,$10}' ; done;
